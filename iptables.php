8339caf4d1daaf317ec1c507f9f1f6a62924e956
root <root@r64.evilbunny.org>
2018-02-15 11:24:38 +1100

    add iptables.php

---
+++ iptables.php
@@ -0,0 +1,27 @@
+<?php
+        session_start();
+
+        if($_SESSION['login'] != true)
+        {
+                header("location: login.php");
+                exit;
+        }
+
+	$page = 8;
+	$pageTitle = "Firewall Settings";
+	include_once("header.php");
+?>
+        <div id="page-wrapper">
+            <div id="page-inner">
+                <div class="row" style="padding-right:15px;padding-left:15px;">
+                    <div class="col-md-12">
+                        <h2><?=$pageTitle?></h2>
+                    </div>
+                </div>
+                <hr />
+		<div class="row" style="padding-right:15px;padding-left:15px;">
+		    Coming soon!
+		</div>
+	    </div>
+        </div>
+<?php include_once("footer.php"); ?>
