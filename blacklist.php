79ff82b45e5d6b567c7b923b231f3a1bd9ff6faf
evilbunny2008 <evilbunny@evilbunny.org>
2018-02-18 12:46:19 +1100

    updated blacklisting script

--- blacklist.php
+++ blacklist.php
@@ -7,9 +7,10 @@
                 exit;
         }
 
-	if(isset($_POST['enableBL']) && $_POST['enableBL'] == "no")
+	if(isset($_POST['button']) && !isset($_POST['enableBL']))
 	{
-		$do = `sudo rm -f /etc/adfree.conf`;
+		$do = `sudo rm -f "/etc/adfree.conf"`;
+		$do = `sudo rm -f "/etc/cron.daily/blacklist.sh"`;
 	}
 
 	if(isset($_POST['button']) && isset($_POST['enableBL']) && $_POST['enableBL'] == "yes")
@@ -19,13 +20,17 @@
 
 		$do = `echo "username=$username\npassphrase=$passphrase" | sudo tee "/etc/adfree.conf"`;
 		$do = `sudo chmod 600 "/etc/adfree.conf"`;
+		$do = `sudo echo "#!/bin/sh\n\n/var/www/html/scripts/update-blacklist.php" | sudo tee "/etc/cron.daily/blacklist.sh"`;
+		$do = `sudo chmod 755 "/etc/cron.daily/blacklist.sh"`;
 	}
 
 	$okmsg = "";
-	if(isset($_POST['dlBlacklist']))
+	if(isset($_POST['dlBlacklist']) && isset($_POST['enableBL']) && $_POST['enableBL'] == "yes")
 	{
-		$do = `sudo "/var/www/html/scripts/update-blacklist.php" > /dev/null 2>/dev/null &`;
-		$okmsg = "The system is now updating the Blacklist.";
+		$do = `sudo "/var/www/html/scripts/update-blacklist.php"`;
+		$okmsg = "The system has now updated the Blacklist.";
+	} else if(isset($_POST['dlBlacklist']) && !isset($_POST['enableBL'])) {
+		$errmsg = "Blacklisting isn't enabled.";
 	}
 
 	$enableBL = "no";
@@ -54,9 +59,13 @@
                 </div>
                 <hr />
 		<div class="row" style="padding-right:15px;padding-left:15px;">
+<?php if($errmsg != "") { ?>
+                    <p><div class="alert alert-warning alert-dismissable"><?=$errmsg?><button type="button" class="close" data-dismiss="alert" aria-hidden="true">x</button></div></p>
+<?php } ?>
 <?php if($okmsg != "") { ?>
                     <p><div class="alert alert-success alert-dismissable"><?=$okmsg?><button type="button" class="close" data-dismiss="alert" aria-hidden="true">x</button></div></p>
 <?php } ?>
+
 		    <form method="post" action="<?=$_SERVER['PHP_SELF']?>">
 			<div style="width:140px;float:left">Enable blacklisting?</div>
 			<input type="checkbox" style="width:25px;float:left;" class="form-control" name="enableBL" value="yes"<?php if($enableBL == "yes") { echo " checked"; } ?> /><br style="clear:left;"/>
@@ -66,7 +75,7 @@
 			<div style="width:140px;float:left">Adfree Passphrase:</div>
 			<input type="text" style="width:200px;float:left;margin-left:20px;" class="form-control" name="passphrase" value="<?=$passphrase?>" placeholder="Adfree passphrase" /><br style="clear:left;"/>
 			<input type="submit" class="btn btn-primary" name="button" value="Save Settings" />
-			<input type="submit" class="btn btn-primary" name="dlBlacklist" value="Download Blacklist" />
+			<input type="submit" class="btn btn-primary" name="dlBlacklist" value="Download Blacklist Now" />
 		    </form>
 		</div>
 	    </div>
