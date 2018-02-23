b349ecd123dc1fffb423ef7e4c67d681fb0c0da5
evilbunny2008 <evilbunny@evilbunny.org>
2018-02-23 23:09:20 +1100

    dnslog update

--- mysql.default.php
+++ mysql.default.php
@@ -1,19 +1,2 @@
 <?php
-	$link = mysqli_connect("localhost", "username", "password", "database");
-
-/*
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-CREATE TABLE `dnslog` (
-  `qid` int(10) UNSIGNED NOT NULL,
-  `when` datetime NOT NULL,
-  `qtype` enum('A','AAAA') NOT NULL,
-  `hostname` varchar(255) NOT NULL,
-  `client` varchar(255) NOT NULL,
-  `status` enum('cached','blocked','forwarded') NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-ALTER TABLE `dnslog`
-  ADD PRIMARY KEY (`qid`,`when`);
-*/
+	$link = mysqli_connect("localhost", "webgui", "password", "webgui");
